ebbecf4ce5e6c6d829a2c9eb2b07227ad362d4f7
junge <l>
2019-01-27 21:51:35 +0800

    做好了路由功能

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import app from './App.vue'
+import router from './router.js'
+import VueRouter from 'vue-router'
+
 import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
 import { Header } from 'mint-ui';
 
+Vue.use(VueRouter)
 Vue.component(Header.name, Header);
+
 var vm=new Vue({
   el:'#app',
-  render:c=>c(app)
+  render:c=>c(app),
+  router//挂载路由对象
 })
--- src/router.js
+++ src/router.js
@@ -1,12 +1,19 @@
 import VueRouter from 'vue-router'
-
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MenberContainer from './components/tabbar/MenberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
 
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-    
-  ]
+    {path:'/home',component:HomeContainer},
+    {path:'/menber',component:MenberContainer},
+    {path:'/shopcar',component:ShopcarContainer},
+    {path:'/search',component:SearchContainer}
+  ],
+  linkActiveClass:' mui-active'
 })
 
 // 把路由对象暴露出去
