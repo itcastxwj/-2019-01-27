ec8cb5d4552b1f40cbf6c2dd96d0439749961885
junge <l>
2019-01-29 15:46:59 +0800

    算了我回来加班了

--- src/components/subcomponents/comment.vue
+++ src/components/subcomponents/comment.vue
@@ -1,65 +1,95 @@
 <template>
   <div>
     <h3>发表评论</h3>
-    <hr>
-    <textarea placeholder="请你多BB几句" maxlength="120"></textarea>
-    <mt-button type="primary" size="large">发表评论</mt-button>
+    <hr>请输入你的姓名:
+    <input type="text" v-model="name">
+    请发表你的评论:
+    <textarea placeholder="请你多BB几句" maxlength="120" v-model="message"></textarea>
+    <mt-button type="primary" size="large" @click="postmsg">发表评论</mt-button>
     <mt-button type="danger" size="large" plain>加载更多</mt-button>
     <div class="comment-list">
-      <div class="comment-item">
-        <div class="up">
-          第一楼&nbsp;&nbsp;用户:匿名用户&nbsp;&nbsp;发表时间:2019/1/29
-        </div>
-        <div class="down">
-          你是一只猪
-        </div>
-      </div>
-       <div class="comment-item">
-        <div class="up">
-          第一楼&nbsp;&nbsp;用户:匿名用户&nbsp;&nbsp;发表时间:2019/1/29
-        </div>
-        <div class="down">
-          你是一只猪
-        </div>
-      </div>
-       <div class="comment-item">
-        <div class="up">
-          第一楼&nbsp;&nbsp;用户:匿名用户&nbsp;&nbsp;发表时间:2019/1/29
-        </div>
-        <div class="down">
-          你是一只猪
-        </div>
-      </div>
-       <div class="comment-item">
-        <div class="up">
-          第一楼&nbsp;&nbsp;用户:匿名用户&nbsp;&nbsp;发表时间:2019/1/29
-        </div>
-        <div class="down">
-          你是一只猪
-        </div>
+      <div class="comment-item" v-for="item in list" :key="item.id">
+        <div
+          class="up"
+        >{{item.id}}&nbsp;&nbsp;用户:{{item.name}}&nbsp;&nbsp;发表时间:{{item.ctime|dateFormat}}</div>
+        <div class="down">{{item.message}}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "",
+      message: "",
+      list: [
+        {
+          id: 1,
+          name: "谢文钧",
+          ctime: new Date(),
+          message: "小芳是猪"
+        },
+        {
+          id: 2,
+          name: "钟芳宁",
+          ctime: new Date(),
+          message: "聪聪是猪"
+        },
+        {
+          id: 3,
+          name: "李聪聪",
+          ctime: new Date(),
+          message: "小芳和聪聪是猪"
+        }
+      ]
+    };
+  },
+  // created() {
+  //   this.postmsg();
+  // },
+  methods: {
+    postmsg() {
+      if (this.name != "" && this.message != "") {
+        this.list.push({
+          id: this.list.length + 1,
+          name: this.name,
+          ctime: new Date(),
+          message: this.message
+        });
+        this.name = this.message = "";
+      }
+    }
+  },
+  filters: {
+    dateFormat() {
+      var dt = new Date()
+      var y = dt.getFullYear()
+      var m = (dt.getMonth() + 1).toString().padStart(2, "0")
+      var d = dt.getDate().toString().padStart(2, "0")
+      var h =dt.getHours().toString().padStart(2, "0")
+      var n=dt.getMinutes().toString().padStart(2,'0')
+      var s=dt.getSeconds().toString().padStart(2,'0')
+      return y + "-" + m + "-" + d+'  '+h+'-'+n+'-'+s 
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-.comment-list{
+.comment-list {
   margin: 10px 0;
-  .comment-item{
+  .comment-item {
     margin-bottom: 10px;
     // text-align: center;
 
-    .up{
+    .up {
       background-color: #ccc;
       font-size: 13px;
     }
-    .down{
-    text-indent: 2em;
+    .down {
+      text-indent: 2em;
       font-size: 16px;
-
     }
   }
 }
